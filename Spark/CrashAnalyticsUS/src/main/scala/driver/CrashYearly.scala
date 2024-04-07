faf228f165a4311ade05f107e567ea18db8c3d48
Yunik Tamrakar <yuniktmr@gmail.com>
2024-04-06 22:34:25 -0600

    Cleaning up column names for sql processing. Also added logic to load dataframes via parquet.

--- Spark/CrashAnalyticsUS/src/main/scala/driver/CrashYearly.scala
+++ Spark/CrashAnalyticsUS/src/main/scala/driver/CrashYearly.scala
@@ -1,7 +1,9 @@
 package driver
 
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.functions._
+import schema.TrafficCollisionSchema
 import store.CrashYearlyDataframe
 
 import scala.collection.mutable
@@ -14,6 +16,7 @@ object CrashYearly {
     Logger.getLogger("akka").setLevel(Level.ERROR)
 
     val df = CrashYearlyDataframe.df
+    val schema = TrafficCollisionSchema.schema
 
     // Find distinct years in the DataFrame
     val years = df.select("Year").distinct().collect().map(_.getInt(0))
@@ -21,11 +24,25 @@ object CrashYearly {
     // Initialize a mutable map to hold DataFrames for each year
     val yearlyDataFrames = mutable.Map[Int, org.apache.spark.sql.DataFrame]()
 
-    // Loop through each year and filter DataFrame by that year
+    // FileSystem setup for checking existence of Parquet files
+    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+
+    // Loop through each year and process DataFrame by that year
     years.foreach { year =>
-      val yearlyDataFrame = df.filter(col("Year") === year)
-      yearlyDataFrames(year) = yearlyDataFrame
-      println(s"Data for $year has ${yearlyDataFrame.count()} rows.")
+      val path = s"src/main/scala/cache/yearly/year_$year"
+      if (fs.exists(new Path(path))) {
+        println(s"Loading data for $year from cache.")
+        //val yearlyDataFrame = spark.read.format("parquet").load(path)
+        val yearlyDataFrame = spark.read.schema(schema).parquet(path)
+        yearlyDataFrames(year) = yearlyDataFrame
+        println(s"Data for $year has ${yearlyDataFrame.count()} rows.")
+      } else {
+        println(s"Data for $year not found in cache, processing and caching.")
+        val yearlyDataFrame = df.filter(col("Year") === year)
+        yearlyDataFrames(year) = yearlyDataFrame
+        yearlyDataFrame.write.format("parquet").save(path)
+        println(s"Data for $year has been written to $path")
+      }
     }
 
     // Example usage: show data for a specific year, e.g., 2019
--- Spark/CrashAnalyticsUS/src/main/scala/store/CrashYearlyDataframe.scala
+++ Spark/CrashAnalyticsUS/src/main/scala/store/CrashYearlyDataframe.scala
@@ -6,6 +6,8 @@ import org.apache.spark.sql.functions._
 object CrashYearlyDataframe {
   val spark = SparkSession.builder()
     .appName("Yearly Crash Data Analytics")
+//    .config("spark.io.compression.codec", "lz4")
+    .config("spark.sql.parquet.compression.codec", "gzip")
     .master("local[*]") // Use local mode with all cores
     .getOrCreate()
 
