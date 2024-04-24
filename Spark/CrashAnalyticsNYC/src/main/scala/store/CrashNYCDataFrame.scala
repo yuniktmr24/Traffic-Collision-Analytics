7a32506b3c4151ee68c45b0772ff3cf5664a3e67
Dilochan Karki <dilochan@tallahassee.cs.colostate.edu>
2024-04-23 18:27:35 -0600

    updated code for cluster setup

--- Spark/CrashAnalyticsNYC/src/main/scala/store/CrashNYCDataFrame.scala
+++ Spark/CrashAnalyticsNYC/src/main/scala/store/CrashNYCDataFrame.scala
@@ -6,13 +6,13 @@ import schema.NYCCollisionSchema
 object CrashNYCDataFrame {
   val spark = SparkSession.builder()
     .appName("NYC Crash Analytics")
-    .master("local[*]") // Use local mode with all cores
+//    .master("local[*]") // Use local mode with all cores
     .getOrCreate()
 
   // Read data using the defined schema
-  var df = spark.read
-    .option("header", "true") // Assumes the first row is a header
-    .schema(NYCCollisionSchema.schema)
-    .csv("src/main/resources/NYC_Motor_Vehicle_Collisions_-_Crashes.csv")
+//  var df = spark.read
+//    .option("header", "true") // Assumes the first row is a header
+//    .schema(NYCCollisionSchema.schema)
+//    .csv("src/main/resources/NYC_Motor_Vehicle_Collisions_-_Crashes.csv")
 
 }
